6a414846c36208adfee3d2911b7d261516b3cf83
Vishal <vishalshinde@541@gmail.com>
2024-02-21 17:52:07 +0530

    Used Qualifiers for UserNotificationServiceModule.kt and created Annotation MessageModifier

--- app/src/main/java/com/example/daggerexample/UserNotificationServiceModule.kt
+++ app/src/main/java/com/example/daggerexample/UserNotificationServiceModule.kt
@@ -3,15 +3,20 @@ package com.example.daggerexample
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class UserNotificationServiceModule {
 
+    @MessageQualifier
     @Provides
     fun getMessageService() : NotificationService {
         return MessageService()
     }
 
-//    @Binds
-//    abstract fun getEmailService(emailService: EmailService) : NotificationService
+    @Named("email")
+    @Provides
+    fun getEmailService(emailService: EmailService) : NotificationService {
+        return EmailService()
+    }
 }
--- app/src/main/java/com/example/daggerexample/UserRegistrationService.kt
+++ app/src/main/java/com/example/daggerexample/UserRegistrationService.kt
@@ -1,10 +1,11 @@
 package com.example.daggerexample
 
 import javax.inject.Inject
+import javax.inject.Named
 
 class UserRegistrationService @Inject constructor (
     private val userRepository: UserRepository,
-    private val notificationService: NotificationService
+    @MessageQualifier private val notificationService: NotificationService
 ) {
 
     fun registerUser(email: String, password: String) {
