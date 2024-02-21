6505f00569453b1f6d099496818c05f24c131cab
Vishal <vishalshinde@541@gmail.com>
2024-02-21 16:42:54 +0530

    use of @Binds annotation

--- app/src/main/java/com/example/daggerexample/UserNotificationServiceModule.kt
+++ app/src/main/java/com/example/daggerexample/UserNotificationServiceModule.kt
@@ -1,13 +1,17 @@
 package com.example.daggerexample
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class UserNotificationServiceModule {
+abstract class UserNotificationServiceModule {
 
-    @Provides
-    fun getMessageService() : NotificationService {
-        return MessageService()
-    }
+//    @Provides
+//    fun getMessageService() : NotificationService {
+//        return MessageService()
+//    }
+
+    @Binds
+    abstract fun getEmailService(emailService: EmailService) : NotificationService
 }
